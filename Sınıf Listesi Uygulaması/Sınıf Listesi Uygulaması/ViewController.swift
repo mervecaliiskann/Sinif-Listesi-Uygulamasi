38d3d52754cf129b66e2f1e2eb989991e50b573d
MerveÇalışkan <mervecaliiskannk@gmail.com>
2024-06-08 18:32:28 +0300

    Sınıf Listesi Uygulaması
    I made a basic application to learn how to add CollectionView and Navigation Controller

---
+++ Sınıf Listesi Uygulaması/Sınıf Listesi Uygulaması/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sınıf Listesi Uygulaması
+//
+//  Created by Merve Çalışkan on 8.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
