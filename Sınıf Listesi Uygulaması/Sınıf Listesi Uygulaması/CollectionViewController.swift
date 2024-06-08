ba416063a5f6fc925d1dd0756263e88ba70e2e30
MerveÇalışkan <mervecaliiskannk@gmail.com>
2024-06-08 20:28:53 +0300

    The return value has changed.
    Instead of 20 cells, cells were created according to the number of students.

--- Sınıf Listesi Uygulaması/Sınıf Listesi Uygulaması/CollectionViewController.swift
+++ Sınıf Listesi Uygulaması/Sınıf Listesi Uygulaması/CollectionViewController.swift
@@ -64,7 +64,7 @@ struct Ogrenci {
         return 1
     }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return ogrenciler.count
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: hucreAdi, for: indexPath)
