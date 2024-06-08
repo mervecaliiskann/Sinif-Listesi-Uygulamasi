b99465b8f76af46a0ceaf48c37a4f67e5f0a7f8b
MerveÇalışkan <mervecaliiskannk@gmail.com>
2024-06-08 18:52:15 +0300

    CollectionView-3
    20 cells have been added to the section on the CollectionView page.

--- Sınıf Listesi Uygulaması/Sınıf Listesi Uygulaması/CollectionViewController.swift
+++ Sınıf Listesi Uygulaması/Sınıf Listesi Uygulaması/CollectionViewController.swift
@@ -2,3 +2,6 @@ private let hucreAdi = "hucreAdi"
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
