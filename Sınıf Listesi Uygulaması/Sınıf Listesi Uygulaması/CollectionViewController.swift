60509f8f4c3e2afa020bd4fac70a03e48f5efc5f
MerveÇalışkan <mervecaliiskannk@gmail.com>
2024-06-08 18:53:50 +0300

    CollectionView-3
    A label written inside the CollectionViewCell was assigned to each cell.

--- Sınıf Listesi Uygulaması/Sınıf Listesi Uygulaması/CollectionViewController.swift
+++ Sınıf Listesi Uygulaması/Sınıf Listesi Uygulaması/CollectionViewController.swift
@@ -5,3 +5,11 @@ private let hucreAdi = "hucreAdi"
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 20
     }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: hucreAdi, for: indexPath)
+        if let costumCell = cell as? CollectionViewCell {
+            costumCell.lblOgrenciAdi.text = "Ogrenci \(indexPath.row)"
+            return costumCell
+        }
+        return cell
+    }
